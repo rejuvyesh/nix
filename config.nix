3e058f6bc1aec5071af32f70dfac224cc9d9c923
rejuvyesh <mail@rejuvyesh.com>
2016-04-05 23:38:28 -0700

    use nix only for haskell

--- config.nix
+++ config.nix
@@ -3,15 +3,8 @@
 packageOverrides = self: with pkgs; rec {
 
 local = let
-  haskellngPackages = pkgs.haskellngPackages.override {
-      overrides = self : super : {
-        pandoc-live = self.callPackage ./packages/pandoc-live.nix {};
-        firewall-auth = self.callPackage ./packages/firewall-auth.nix {};
-      };    
-  };
 
-  hs  = haskellngPackages;
-  py3 = python3Packages;
+  hs  = haskellPackages;
 
   zathura = stdenv.lib.overrideDerivation pkgs.zathuraCollection.zathuraWrapper (_: {
     zathura_core = stdenv.lib.overrideDerivation pkgs.zathuraCollection.zathuraWrapper.zathura_core (_ : {
@@ -23,307 +16,50 @@ local = let
       buildInputs = [pkgs.python27Packages.sphinx];
     });
   });
-  
+
 in recurseIntoAttrs rec {
    # standard environment
   base = hiPrio (pkgs.buildEnv {
     name = "rejuvnix";
     paths = [
       # nix-related
-      gem-nix
       nix-prefetch-scripts
       nix-repl
       nox
-
-      # archives
-      bchunk
-      gnutar
-      libarchive
-      pigz
-      p7zip
-      rpm
-      unrar
-      unzipNLS
-
-      # stuff
-      mc
-      parallel
-      reptyr
-      rlwrap
-      tmux
-      tzdata
-      zsh
-      rxvt_unicode
-
-      # system
-      extundelete
-      hddtemp
-      inotifyTools
-      lsof
-      netcat
-      nmap
-      utillinuxCurses
-      vnstat
     ];
   });
 
-  
+
   myhs = hs.ghcWithPackages (
     pkgs: with pkgs; [
       cabal2nix
       cabal-install
       ghc-mod
       hlint
-      xmonad
-      xmonad-contrib
-      xmonad-extras
-      xmonad
-      xdg-basedir
-      xmobar
-      libmpd
+      hledger
+      hledger-web
+      hserv
+      gitit
+      pandoc
+      # xmonad
+      # xmonad-contrib
+      # xmonad-extras
+      # xmonad
+      # xdg-basedir
+      # xmobar
+      # libmpd
     ]
   );
-  
+
   haskell = hiPrio (pkgs.buildEnv {
     name = "rejuvnix-haskell";
     paths = [
       myhs
     ];
   });
-
-  go = pkgs.buildEnv {
-    name = "rejuvnix-go";
-    paths = [
-      pkgs.go
-    ];
-  };
-
-  java = pkgs.buildEnv {
-    name = "rejuvnix-java";
-    paths = [
-      icedtea7_jdk
-    ];
-  };
-
-  js = pkgs.buildEnv {
-    name = "rejuvnix-js";
-    paths = [
-      nodejs
-    ];
-  };
-
-  julia = pkgs.buildEnv {
-    name = "rejuvnix-julia";
-    paths = [
-      pkgs.julia
-    ];
-  };
-
-  c = pkgs.buildEnv {
-    name = "rejuvnix-c";
-    paths = [
-      # gcc
-      gdb
-      valgrind
-    ];
-  };
-  
-  science = pkgs.myEnvFun {
-    name = "science";
-    buildInputs = [
-      python3env
-      py3.ipython
-      py3.ipdb
-      py3.matplotlib
-      py3.numpy
-      py3.pillow
-      py3.pandas
-      py3.scikitlearn
-      py3.scipy
-    ];
-  };
-  
-  python3 = lowPrio (pkgs.buildEnv {
-    name = "rejuvnix-python3";
-    paths = [
-      python3env
-    ];
-  });
-  
-  rust = pkgs.buildEnv {
-    name = "rejuvnix-rust";
-    paths = [
-      cargoSnapshot
-      rustcMaster
-    ];
-  };
-
-  office = pkgs.buildEnv {
-    name = "rejuvnix-office";
-    paths = [
-      libreoffice
-      unoconv
-    ];
-  };
-
-  wm = hiPrio (pkgs.buildEnv {
-    name = "rejuvnix-wm";
-    paths = [
-      compton
-      dmenu
-      # wmname
-      hs.arbtt
-      
-      gtk_engines
-      gtk-engine-murrine
-    ];
-  });
-
-  code = pkgs.buildEnv {
-    name = "rejuvnix-code";
-    paths = [
-      cloc
-      gperftools
-      silver-searcher
-      strace
-      graphviz
-
-      # db
-      sqliteInteractive
-      sqliteman
-    ];
-  };
-
-  vcs = pkgs.buildEnv {
-    name = "rejuvnix-vcs";
-    paths = [
-      cvs
-      bazaar
-      darcs
-      gitFull
-      gitAndTools.darcsToGit
-      gitAndTools.gitAnnex
-      mercurial
-      subversion
-    ];
-  };
-
-  txt = pkgs.buildEnv {
-    name = "rejuvnix-txt";
-    paths = [
-      calibre
-      zathura
-      pdfgrep
-      
-      colordiff
-      convmv
-      dos2unix
-      ghostscript
-
-      htmlTidy
-      meld
-      pdftk
-      wdiff
-
-      hs.stylish-haskell
-      hs.pandoc-citeproc
-      hs.pandoc-live
-      hs.hindent
-
-      emacs
-    ];
-  };
-
-  track = lowPrio (pkgs.buildEnv {
-    name = "rejuvnix-track";
-    paths = [
-      hs.hledger
-      hs.hledger-web
-    
-      hs.gitit
-    ];
-  });
-  
-  web = pkgs.buildEnv {
-    name = "rejuvnix-web";
-    paths = [
-      aria2
-      dropbox-cli
-      eiskaltdcpp
-      firefoxWrapper
-      mu
-      offlineimap
-      quvi
-      rtmpdump
-      torbrowser
-      weechat
-      awscli
-      hs.hserv
-      hs.firewall-auth
-    ];
-  };
-
-  video = pkgs.buildEnv {
-    name = "rejuvnix-video";
-    paths = [
-      guvcview
-      mpv
-      swftools
-    ];
-  };
-
-  audio = pkgs.buildEnv {
-    name = "rejuvnix-audio";
-    paths = [
-      audacity
-      alsaLib
-      alsaPlugins
-      alsaUtils
-      mpc
-      mpdcron
-      hs.vimus
-    ];
-  };
-
-  image = pkgs.buildEnv {
-    name = "rejuvnix-image";
-    paths = [
-      gimp
-      gimpPlugins.lqrPlugin
-      inkscape
-      imagemagick
-      mcomix
-      scrot
-    ];
-  };
-
-  win = pkgs.buildEnv {
-    name = "rejuvnix-win";
-    paths = [
-      wine
-      winetricks
-    ];
-  };
-
-  python3env = pkgs.python3.buildEnv.override {
-    extraLibs = [
-      py3.beautifulsoup4
-      py3.lxml
-      py3.mock
-      py3.requests
-    ];
-  };
-  
-  
-  www = pkgs.myEnvFun {
-    name = "www";
-    buildInputs = [
-      hs.hakyll 
-    ];
-  };
 };
 
-};  
+};
 
 # general options
 allowUnfree = true;
